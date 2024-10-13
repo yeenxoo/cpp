011f017b48e9a79eda36a0c487da2daca845326f
Enzoo <yeenxoo@gmail.com>
2024-10-13 14:58:49 +0800

    Update n129.cpp

--- ZeroJudge/n129.cpp
+++ ZeroJudge/n129.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, i;
+    cin >> n;
+    vector<int> a(n+3);
+    a[0]=1;
+    a[1]=2;
+    a[2]=4;
+    for(i=3;i<n;i++){
+        a[i]=a[i-1]+a[i-2]+a[i-3];
+    }
+    cout << a[n-1] << endl;
+
+}
 
