3cf053f5a706c937c1c2ba28cb0be24aa55a1016
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-15 17:55:16 +0800

    Update n129.cpp

--- ZeroJudge/n129.cpp
+++ ZeroJudge/n129.cpp
@@ -9,11 +9,11 @@ int main() {
     cin >> n;
     vector<long long> a(n+3);
     a[0]=1;
-    a[1]=2;
-    a[2]=4;
-    for(i=3;i<n;i++){
+    a[1]=1;
+    a[2]=2;
+    for(i=3;i<=n;i++){
         a[i]=a[i-1]+a[i-2]+a[i-3];
     }
-    cout << a[n-1] << endl;
+    cout << a[n] << endl;
 
 }
