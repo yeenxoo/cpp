fba2266f71acd3b33fa778f93f1b1a406784bfd2
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-15 20:00:11 +0800

    Create n368.cpp

---
+++ ZeroJudge/n368.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool cmp(long long x, long long y){
+	return x>y;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long n, a, b, i, sum=0;
+    cin >> n;
+    vector<long long> numbers;
+    for(i=0;i<n;i++){
+    	cin >> a >> b;
+    	if(max(a,b)>0) numbers.push_back(max(a,b));
+    }
+    sort(numbers.begin(),numbers.end(),cmp);
+    for(i=0;i<n;i++){
+    	if(i<numbers.size()) sum+=numbers[i];
+    	cout << sum << "\n";
+    }
+    
+}
