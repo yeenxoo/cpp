ebc152ba6fa1a169aa36d87bebecbf0fa5408a7c
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-16 14:17:04 +0800

    Update d212.cpp

--- ZeroJudge/d212.cpp
+++ ZeroJudge/d212.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long long n, i;
+    while(cin >> n){
+        vector<long long> dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        cout << dp.back() << "\n";
+    }
+}
