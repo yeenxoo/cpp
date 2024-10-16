69d8048b13bfc5b651f521204043074d507269ec
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-16 18:05:41 +0800

    Create n369(map).cpp

---
+++ ZeroJudge/n369(map).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    map<string,bool> mail;
+    string a, b;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        if(mail[a]==0){
+            cout << "welcome, " << b << "\n";
+            mail[a]=true;
+        }
+        else cout << b << " account has been used\n";
+    }
+}
