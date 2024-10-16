0f78325aa1c198419faf77a46178888cbce79146
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-16 18:59:02 +0800

    Update 板中112校內賽1B.cpp

--- CodeForces/板中112校內賽1B.cpp
+++ CodeForces/板中112校內賽1B.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long long n;
+    string a, b;
+    unordered_map<string,string> m;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        m[a]=b;
+    }
+    while(cin>>a){
+        cout << m[a] << " ";
+    }
+}
