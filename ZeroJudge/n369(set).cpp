2e3da86a3fc98baed10500dcc0cc67ebd6ac0b16
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-16 18:04:24 +0800

    Create n369(set).cpp

---
+++ ZeroJudge/n369(set).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    set<string> mail;
+    string a, b;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        if(mail.find(a)==mail.end()){
+            cout << "welcome, " << b << "\n";
+            mail.insert(a);
+        }
+        else cout << b << " account has been used\n";
+    }
+}
