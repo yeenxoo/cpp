876c5f0f09b21ebf322d2990e6ddb1cfd2f9bcfc
Enzoo <181806247+yeenxoo@users.noreply.github.com>
2024-10-16 16:44:38 +0800

    Create e838.cpp

---
+++ ZeroJudge/e838.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n, i, j;
+    cin >> n;
+    string s, bomb(n,'*');
+    vector <bool> col(n,0), row(n,0);
+    cin.ignore();
+    for(i=0;i<n;i++){
+        getline(cin,s);
+        if(s.find('*')!=string::npos){
+            row[i]=1;
+            for(j=0;j<n;j++){
+                if(s[j]=='*') col[j]=1;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        if(row[i]==1) cout << bomb;
+        else{
+            for(j=0;j<n;j++){
+                if(col[j]==1) cout << '*';
+                else cout << '0';
+            }
+        }
+        cout << "\n";
+    }
+}
